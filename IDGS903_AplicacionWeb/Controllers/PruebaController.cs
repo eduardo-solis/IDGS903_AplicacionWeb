6af4659fc55da9b4443e52ec9520283cec79311c
eduardo-solis <solishernandezeduardoruben@gmail.com>
2022-05-25 17:20:39 -0500

    Conversion de celsius a fahrenheit

--- IDGS903_AplicacionWeb/Controllers/PruebaController.cs
+++ IDGS903_AplicacionWeb/Controllers/PruebaController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IDGS903_AplicacionWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,54 @@ namespace IDGS903_AplicacionWeb.Controllers
         {
             return View();
         }
+
+        public ActionResult Opera()
+        {
+            OperasBas op = new OperasBas();
+
+            return View(op);
+        }
+
+        [HttpPost]
+        public ActionResult Opera(OperasBas op, string r1)
+        {
+            
+            if(r1.Equals("Sumar"))
+            {
+                op.Suma();
+            }
+            if (r1.Equals("Restar"))
+            {
+                op.Resta();
+            }
+
+            return View(op);
+        }
+
+
+        public ActionResult Grados()
+        {
+            ConversionGrados cg = new ConversionGrados();
+            return View(cg);
+        }
+
+        [HttpPost]
+        public ActionResult Grados(ConversionGrados cg)
+        {
+            if (cg.opcion.Equals("C"))
+            {
+                cg.ConvertirACelcius();
+            }
+
+            if (cg.opcion.Equals("F"))
+            {
+                cg.ConvertirAFahrenheit();
+            }
+
+
+            return View(cg);
+        }
+
 
         // GET: Prueba/Details/5
         public ActionResult Details(int id)
---
+++ IDGS903_AplicacionWeb/Models/ConversionGrados.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IDGS903_AplicacionWeb.Models
+{
+    public class ConversionGrados
+    {
+
+        public double Grado { get; set; }
+        public string Res { get; set; }
+        public string opcion { get; set; }
+
+        public void ConvertirAFahrenheit()
+        {
+            this.Res = ((this.Grado * 9) / 5) + 32 + "°F";
+        }
+
+        public void ConvertirACelcius()
+        {
+            this.Res = ((this.Grado - 32) * 5 )/ 9 + "°C";
+        }
+
+
+
+
+    }
+}
